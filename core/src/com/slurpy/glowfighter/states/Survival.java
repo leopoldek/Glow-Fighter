8ad8bd910d3c31af50dbefef965ef23fea750572
Daniel Eliasinski <danieleliasinski@msn.com>
2017-11-19 02:26:11 -0500

    Merged SurvivalGui and Survival State

--- core/src/com/slurpy/glowfighter/states/Survival.java
+++ core/src/com/slurpy/glowfighter/states/Survival.java
@@ -31,14 +31,12 @@ import com.slurpy.glowfighter.utils.Util;
 import com.slurpy.glowfighter.utils.tasks.KeyFrame;
 import com.slurpy.glowfighter.utils.tasks.TaskBuilder;
 
-public class Survival implements State{
+public class Survival implements State, Gui{
 	
 	private static final float length = 70;
 	private static final float height = 50;
 	private static final float wallWidth = 5;
 	private static final float spawnRange = Math.min(length, height) - 5f;
-	
-	private final SurvivalGui gui;
 	
 	private Player player;
 	
@@ -49,8 +47,24 @@ public class Survival implements State{
 	
 	private GunPickup pickup;
 	
+	//Gui
+	private static final float INDICATOR_SIZE = 15;
+	
+	private final Position healthBarPos = new Position(1, 0, -50, 50);
+	private final Position gunStatsPos = new Position(0, 0, 50, 50);
+	private final Position fpsPos = new Position(0, 1, 10, -10);
+	private final Position levelPos = new Position(0.5f, 1, -40, -10);
+	
+	private final PolygonPart arrowIndicator = new PolygonPart(new Vector2[]{
+			new Vector2(INDICATOR_SIZE, 0),
+			new Vector2(-INDICATOR_SIZE, -INDICATOR_SIZE),
+			new Vector2(-INDICATOR_SIZE, INDICATOR_SIZE)
+	}, 6);
+	
+	private float levelTextSize = 32;
+	
 	public Survival(){
-		gui = new SurvivalGui();
+		
 	}
 
 	@Override
@@ -91,7 +105,7 @@ public class Survival implements State{
 			accumulator -= timer;
 			ticks++;
 			if(ticks > 200){
-				gui.animateNextLevel();
+				animateNextLevel();
 				ticks = 0;
 			}
 		}
@@ -123,112 +137,91 @@ public class Survival implements State{
 		pickup = new GunPickup(Util.randomTriangularVector(spawnRange), gun);
 		Core.entities.addEntity(pickup);
 	}
-
+	
 	@Override
-	public Gui getGui(){
-		return gui;
+	public void draw(){
+		if(!Constants.SHOW_GUI)return;
+		if(Constants.SHOW_INDICATOR){
+			Vector2 playerPos = Core.graphics.project(player.getPosition());
+			Vector2 pickupPos = Core.graphics.project(pickup.getPosition());
+			if(!Util.isInsideRect(playerPos, pickupPos, Gdx.graphics.getWidth(), Gdx.graphics.getHeight())){
+				Vector2 indicatorPos = Util.getBoundryPoint(playerPos, pickupPos, Gdx.graphics.getWidth() - 40, Gdx.graphics.getHeight() - 40);
+				arrowIndicator.draw(indicatorPos, pickupPos.sub(playerPos).angleRad(), Color.LIME);
+			}
+		}
+		
+		Vector2 start = healthBarPos.getPosition();
+		float health = player.getHealth();
+		Vector2 end = new Vector2(start).add(health * -2, 0);
+		Core.graphics.drawLine(start, end, 20f, health <= 0 ? Color.GRAY : Color.RED);
+		
+		start = gunStatsPos.getPosition();
+		if(!player.usingDefault()){
+			Gun gun = player.getGun();
+			float time = gun.getTimeLeft() / gun.maxTime;
+			end.set(start).add(time * 200, 0);
+			Core.graphics.drawLine(start, end, 20f, Color.RED.cpy().lerp(Color.CHARTREUSE, time));
+			Core.graphics.drawText(gun.getName(), start.add(0, 40), 32, Color.NAVY);
+		}
+		
+		if(Constants.SHOW_FPS)Core.graphics.drawText(Integer.toString(Gdx.graphics.getFramesPerSecond()), fpsPos.getPosition(), 24, Color.GRAY);
+		Core.graphics.drawText("Level " + level, levelPos.getPosition(), levelTextSize, Color.WHITE);
 	}
 	
-	public class SurvivalGui implements Gui{
-		private static final float INDICATOR_SIZE = 15;
-		
-		private final Position healthBarPos = new Position(1, 0, -50, 50);
-		private final Position gunStatsPos = new Position(0, 0, 50, 50);
-		private final Position fpsPos = new Position(0, 1, 10, -10);
-		private final Position levelPos = new Position(0.5f, 1, -40, -10);
-		
-		private final PolygonPart arrowIndicator = new PolygonPart(new Vector2[]{
-				new Vector2(INDICATOR_SIZE, 0),
-				new Vector2(-INDICATOR_SIZE, -INDICATOR_SIZE),
-				new Vector2(-INDICATOR_SIZE, INDICATOR_SIZE)
-		}, 6);
-		
-		private float levelTextSize = 32;
-		
-		public SurvivalGui(){
-			
-		}
-		
-		@Override
-		public void draw(){
-			if(!Constants.SHOW_GUI)return;
-			if(Constants.SHOW_INDICATOR){
-				Vector2 playerPos = Core.graphics.project(player.getPosition());
-				Vector2 pickupPos = Core.graphics.project(pickup.getPosition());
-				if(!Util.isInsideRect(playerPos, pickupPos, Gdx.graphics.getWidth(), Gdx.graphics.getHeight())){
-					Vector2 indicatorPos = Util.getBoundryPoint(playerPos, pickupPos, Gdx.graphics.getWidth() - 40, Gdx.graphics.getHeight() - 40);
-					arrowIndicator.draw(indicatorPos, pickupPos.sub(playerPos).angleRad(), Color.LIME);
-				}
+	@Override
+	public Gui getGui(){
+		return this;
+	}
+	
+	public void animateNextLevel(){
+		TaskBuilder builder = new TaskBuilder();
+		final float ryStart = 1f;
+		final float ryEnd = 0.5f;
+		final float xStart = -40;
+		final float xEnd = -55;
+		final float yStart = -10;
+		final float yEnd = 10;
+		final float textSizeStart = 32;
+		final float textSizeEnd = 58;
+		builder.addKeyFrame(new KeyFrame(){
+			@Override
+			public void act(float progress, float frameProgress) {
+				levelPos.ry = Interpolation.circleOut.apply(ryStart, ryEnd, frameProgress);
+				levelPos.x = Interpolation.circleOut.apply(xStart, xEnd, frameProgress);
+				levelPos.y = Interpolation.linear.apply(yStart, yEnd, frameProgress);
+				levelTextSize = Interpolation.circleOut.apply(textSizeStart, textSizeEnd, frameProgress);
 			}
-			
-			Vector2 start = healthBarPos.getPosition();
-			float health = player.getHealth();
-			Vector2 end = new Vector2(start).add(health * -2, 0);
-			Core.graphics.drawLine(start, end, 20f, health <= 0 ? Color.GRAY : Color.RED);
-			
-			start = gunStatsPos.getPosition();
-			if(!player.usingDefault()){
-				Gun gun = player.getGun();
-				float time = gun.getTimeLeft() / gun.maxTime;
-				end.set(start).add(time * 200, 0);
-				Core.graphics.drawLine(start, end, 20f, Color.RED.cpy().lerp(Color.CHARTREUSE, time));
-				Core.graphics.drawText(gun.getName(), start.add(0, 40), 32, Color.NAVY);
+			@Override
+			public void end() {
+				levelPos.ry = ryEnd;
+				levelPos.x = xEnd;
+				levelPos.y = yEnd;
+				levelTextSize = textSizeEnd;
+				
+				level++;
+				
+				Core.entities.addEntity(new BallLaunchingEnemy(new Vector2(spawnRange, 0)));
+				Core.entities.addEntity(new BallLaunchingEnemy(new Vector2(-spawnRange, 0)));
+				if(level % 5 == 0)Core.entities.addEntity(new TurretEnemy(new Vector2(), player));
 			}
-			
-			if(Constants.SHOW_FPS)Core.graphics.drawText(Integer.toString(Gdx.graphics.getFramesPerSecond()), fpsPos.getPosition(), 24, Color.GRAY);
-			Core.graphics.drawText("Level " + level, levelPos.getPosition(), levelTextSize, Color.WHITE);
-		}
-		
-		public void animateNextLevel(){
-			TaskBuilder builder = new TaskBuilder();
-			final float ryStart = 1f;
-			final float ryEnd = 0.5f;
-			final float xStart = -40;
-			final float xEnd = -55;
-			final float yStart = -10;
-			final float yEnd = 10;
-			final float textSizeStart = 32;
-			final float textSizeEnd = 58;
-			builder.addKeyFrame(new KeyFrame(){
-				@Override
-				public void act(float progress, float frameProgress) {
-					levelPos.ry = Interpolation.circleOut.apply(ryStart, ryEnd, frameProgress);
-					levelPos.x = Interpolation.circleOut.apply(xStart, xEnd, frameProgress);
-					levelPos.y = Interpolation.linear.apply(yStart, yEnd, frameProgress);
-					levelTextSize = Interpolation.circleOut.apply(textSizeStart, textSizeEnd, frameProgress);
-				}
-				@Override
-				public void end() {
-					levelPos.ry = ryEnd;
-					levelPos.x = xEnd;
-					levelPos.y = yEnd;
-					levelTextSize = textSizeEnd;
-					
-					level++;
-					
-					Core.entities.addEntity(new BallLaunchingEnemy(new Vector2(spawnRange, 0)));
-					Core.entities.addEntity(new BallLaunchingEnemy(new Vector2(-spawnRange, 0)));
-					if(level % 5 == 0)Core.entities.addEntity(new TurretEnemy(new Vector2(), player));
-				}
-			}, 1.1f);
-			builder.addKeyFrame(1.2f);
-			builder.addKeyFrame(new KeyFrame(){
-				@Override
-				public void act(float progress, float frameProgress) {
-					levelPos.ry = Interpolation.circleOut.apply(ryEnd, ryStart, frameProgress);
-					levelPos.x = Interpolation.circleOut.apply(xEnd, xStart, frameProgress);
-					levelPos.y = Interpolation.linear.apply(yEnd, yStart, frameProgress);
-					levelTextSize = Interpolation.circleOut.apply(textSizeEnd, textSizeStart, frameProgress);
-				}
-				@Override
-				public void end() {
-					levelPos.ry = ryStart;
-					levelPos.x = xStart;
-					levelPos.y = yStart;
-					levelTextSize = textSizeStart;
-				}
-			}, 1.2f);
-			Core.tasks.addTask(builder);
-		}
+		}, 1.1f);
+		builder.addKeyFrame(1.2f);
+		builder.addKeyFrame(new KeyFrame(){
+			@Override
+			public void act(float progress, float frameProgress) {
+				levelPos.ry = Interpolation.circleOut.apply(ryEnd, ryStart, frameProgress);
+				levelPos.x = Interpolation.circleOut.apply(xEnd, xStart, frameProgress);
+				levelPos.y = Interpolation.linear.apply(yEnd, yStart, frameProgress);
+				levelTextSize = Interpolation.circleOut.apply(textSizeEnd, textSizeStart, frameProgress);
+			}
+			@Override
+			public void end() {
+				levelPos.ry = ryStart;
+				levelPos.x = xStart;
+				levelPos.y = yStart;
+				levelTextSize = textSizeStart;
+			}
+		}, 1.2f);
+		Core.tasks.addTask(builder);
 	}
 }
